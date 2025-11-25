54e50339c7a55df9a2db56bd2b8d3475e877ef1d
Patrick Zulian <patrick.zulian@gmail.com>
2025-11-25 22:21:38 +0100

    ...

--- narrowphase.hpp
+++ narrowphase.hpp
@@ -1,58 +1,58 @@
-#ifndef NARROWPHASE_HPP
-#define NARROWPHASE_HPP
+// #ifndef NARROWPHASE_HPP
+// #define NARROWPHASE_HPP
 
-#include "snumerr.hpp"
-#include "snumtol.hpp"
-#include "stuv.hpp"
-#include <limits>
+// // #include "snumerr.hpp"
+// // #include "snumtol.hpp"
+// // #include "stuv.hpp"
+// // #include <limits>
 
-namespace sccd {
+// // namespace sccd {
 
-enum class CCDStepResult {
-  Continue = 0,
-  NoInclusion,
-  DomainTolSatisfied,
-  BoxInside,
-  CodomainTolSatisfied,
-  DegenerateSplit
-};
+// // enum class CCDStepResult {
+// //   Continue = 0,
+// //   NoInclusion,
+// //   DomainTolSatisfied,
+// //   BoxInside,
+// //   CodomainTolSatisfied,
+// //   DegenerateSplit
+// // };
 
-template <typename T>
-int ccd_ee(const int max_iter, const T codomain_tol, const T ms,
-           const int use_ms, const int allow_zero_toi,
-           // Boxes
-           // Start
-           const T v0_sx, const T v0_sy, const T v0_sz,
-           // V1
-           const T v1_sx, const T v1_sy, const T v1_sz,
-           // V2
-           const T v2_sx, const T v2_sy, const T v2_sz,
-           // V3
-           const T v3_sx, const T v3_sy, const T v3_sz,
-           // End
-           // V0
-           const T v0_ex, const T v0_ey, const T v0_ez,
-           // V1
-           const T v1_ex, const T v1_ey, const T v1_ez,
-           // V2
-           const T v2_ex, const T v2_ey, const T v2_ez,
-           // V3
-           const T v3_ex, const T v3_ey, const T v3_ez,
-           // Output
-           T *const toi) {
+// // template <typename T>
+// // int ccd_ee(const int max_iter, const T codomain_tol, const T ms,
+// //            const int use_ms, const int allow_zero_toi,
+// //            // Boxes
+// //            // Start
+// //            const T v0_sx, const T v0_sy, const T v0_sz,
+// //            // V1
+// //            const T v1_sx, const T v1_sy, const T v1_sz,
+// //            // V2
+// //            const T v2_sx, const T v2_sy, const T v2_sz,
+// //            // V3
+// //            const T v3_sx, const T v3_sy, const T v3_sz,
+// //            // End
+// //            // V0
+// //            const T v0_ex, const T v0_ey, const T v0_ez,
+// //            // V1
+// //            const T v1_ex, const T v1_ey, const T v1_ez,
+// //            // V2
+// //            const T v2_ex, const T v2_ey, const T v2_ez,
+// //            // V3
+// //            const T v3_ex, const T v3_ey, const T v3_ez,
+// //            // Output
+// //            T *const toi) {
 
-	// This can be vectorized?
-  int box_in = 0;
-  const int ok = sccd_origin_in_inclusion_ee_soa<T>(
-      t_l, t_u, u_l, u_u, v_l, v_u, v0sx, v0sy, v0sz, v1sx, v1sy, v1sz, v2sx,
-      v2sy, v2sz, v3sx, v3sy, v3sz, v0ex, v0ey, v0ez, v1ex, v1ey, v1ez, v2ex,
-      v2ey, v2ez, v3ex, v3ey, v3ez, ms, ms, ms, errx, erry, errz, &true_tol,
-      &box_in);
+// // 	// This can be vectorized?
+// //   int box_in = 0;
+// //   const int ok = sccd_origin_in_inclusion_ee_soa<T>(
+// //       t_l, t_u, u_l, u_u, v_l, v_u, v0sx, v0sy, v0sz, v1sx, v1sy, v1sz, v2sx,
+// //       v2sy, v2sz, v3sx, v3sy, v3sz, v0ex, v0ey, v0ez, v1ex, v1ey, v1ez, v2ex,
+// //       v2ey, v2ez, v3ex, v3ey, v3ez, ms, ms, ms, errx, erry, errz, &true_tol,
+// //       &box_in);
 
 
-  if (!ok) {
-    return CCDStepResult::NoInclusion;
-  }
-}
+// //   if (!ok) {
+// //     return CCDStepResult::NoInclusion;
+// //   }
+// // }
 
-} // namespace sccd
+// // } // namespace sccd
