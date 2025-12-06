ac086cdc777d08cf8692148e5969948165410296
Patrick Zulian <patrick.zulian@gmail.com>
2025-12-06 13:52:17 +0100

    More info

--- test.sh
+++ test.sh
@@ -7,13 +7,18 @@ source data/venv/bin/activate
 cd build
 make -j8
 
-db_dir=/Users/patrickzulian/Desktop/code/installations/sources/Scalable-CCD/tests/data
-datasets=(armadillo-rollers  cloth-ball cloth-funnel n-body-simulation puffer-ball rod-twist)
+# db_dir=/Users/patrickzulian/Desktop/code/installations/sources/Scalable-CCD/tests/data
+# datasets=(armadillo-rollers cloth-ball cloth-funnel puffer-ball rod-twist n-body-simulation)
+
+db_dir="data"
+datasets=(armadillo-rollers)
 
 cd -
 
 for ds in ${datasets[@]}
 do
+	echo "---------------------------------------------------------"
 	echo "Testing on $ds"
 	SCCD_LIB_PATH=build/libsccd.dylib python3 ccd_test.py $db_dir/$ds
+	echo "---------------------------------------------------------"
 done
