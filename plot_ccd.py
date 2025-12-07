9d95da028d171edbab20738bb36533ca71d8294b
Patrick Zulian <patrick.zulian@gmail.com>
2025-12-07 16:00:33 +0100

    ...

--- plot_ccd.py
+++ plot_ccd.py
@@ -6,27 +6,24 @@ import pandas as pd
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2:
-        print("Usage: python plot_ccd.py <table.csv>")
+    if len(sys.argv) != 3:
+        print("Usage: python plot_ccd.py <name> <table.csv>")
         sys.exit(1)
-    table_file = sys.argv[1]
+    name = sys.argv[1]
+    table_file = sys.argv[2]
     # read the table.csv file
     table = pd.read_csv(table_file)
     # plot the table
 
     expected_hits = table["expected_hit"]
     hits = table["hit"]
-    toi = table["toi"]
-    expected_toi = table["expected_toi"]
+
+    toi = table["toi"][expected_hits].to_numpy()
+    expected_toi = table["expected_toi"][expected_hits].to_numpy()
     time = table["time"]
 
-    diff = np.abs(toi[expected_hits] - expected_toi[expected_hits])
-    hit_diff = expected_hits != hits
+    diff = expected_toi - toi
 
-    print(f"Number of hit differences: {np.count_nonzero(hit_diff)}")
-    print(f"Number of False Positives: {np.count_nonzero(hits[hit_diff])}")
-    print(f"Number of False Negatives: {np.count_nonzero(~hits[hit_diff])}")
-    print(f"Queries per second {len(time)}/{time.sum():.5f} = {len(time)/time.sum():.5f} [qxs]")
 
     print(f'Mean:   {np.mean(diff)}')
     print(f'Std:    {np.std(diff)}')
@@ -37,15 +34,28 @@ if __name__ == "__main__":
     # TODO create a histogram of the difference semilogy
     plt.hist(diff, bins=100, log=True)
     plt.ylabel("Frequency")
-    plt.xlabel("Difference Between TOI and Expected TOI")
+    plt.xlabel("Difference Between Expected TOI and TOI")
     plt.title("Time of Impact (TOI) Difference Histogram")
-    plt.savefig("diff_histogram.pdf")
-    plt.show()
+    plt.savefig(f"{name}_diff_histogram.pdf")
+    # plt.show()
 
-    # plt.plot(diff)
-    plt.semilogy(diff)
-    plt.ylabel("Difference Between TOI and Expected TOI")
-    plt.xlabel("Query")
+    ind = np.argsort(expected_toi)
+    # plt.semilogy(diff)
+    
+    plt.plot(toi[ind], label="TOI")
+    plt.plot(expected_toi[ind], label="Expected TOI")
+    plt.plot(diff[ind], label="Difference")
+    plt.legend()
+    plt.yscale("log")
+    plt.ylabel("Difference Between Expected TOI and TOI")
+    plt.xlabel("Query (sorted by Expected TOI)")
     plt.title("Time of Impact (TOI) Difference")
-    plt.savefig("diff.pdf")
-    plt.show()
+    plt.savefig(f"{name}_diff.pdf")
+    # plt.show()
+
+    hit_diff = expected_hits != hits
+
+    print(f"Number of hit differences: {np.count_nonzero(hit_diff)}")
+    print(f"Number of False Positives: {np.count_nonzero(hits[hit_diff])}")
+    print(f"Number of False Negatives: {np.count_nonzero(~hits[hit_diff])}")
+    print(f"Queries per second {len(time)}/{time.sum():.5f} = {len(time)/time.sum():.5f} [qxs]")
