a66d8f52944b7b128f5ec71d6f610d72d918315c
Patrick Zulian <patrick.zulian@gmail.com>
2025-12-07 17:41:56 +0100

    ...

--- narrowphase.hpp
+++ narrowphase.hpp
@@ -27,6 +27,11 @@ namespace sccd {
         int USE_TI = 0;
         SFEM_READ_ENV(USE_TI, atoi);
 
+        int SCCD_MAX_ITER = 15;
+        SFEM_READ_ENV(SCCD_MAX_ITER, atoi);
+
+        double tol = 1e-12;
+
         tbb::parallel_for(tbb::blocked_range<size_t>(0, noverlaps), [&](const tbb::blocked_range<size_t>& r) {
             std::vector<Box<double>> stack;
             for (size_t i = r.begin(); i < r.end(); i++) {
@@ -51,14 +56,12 @@ namespace sccd {
                 double t = 0;
                 double u = 0;
                 double v = 0;
-                double tol = 1e-12;
-                int max_iter = 15;
 
 #ifdef SCCD_ENABLE_TIGHT_INCLUSION
 #warning "SCCD_ENABLE_TIGHT_INCLUSION"
                 if (USE_TI) {
                     if (find_root_tight_inclusion_vf<double>(
-                            max_iter * 1000, tol, sv, s1, s2, s3, ev, e1, e2, e3, t, u, v)) {
+                            SCCD_MAX_ITER * 1000, tol, sv, s1, s2, s3, ev, e1, e2, e3, t, u, v)) {
                         toi[i] = t;
                         min_t = sccd::min<T>(t, min_t);
                     } else {
@@ -67,8 +70,8 @@ namespace sccd {
                     continue;
                 }
 #endif
-                if (find_root_grid_vf<double>(max_iter, tol, sv, s1, s2, s3, ev, e1, e2, e3, t, u, v, stack))
-                // if (find_root_bisection<double>(max_iter * 3*3*3, tol, sv,
+                if (find_root_grid_vf<double>(SCCD_MAX_ITER, tol, sv, s1, s2, s3, ev, e1, e2, e3, t, u, v, stack))
+                // if (find_root_bisection<double>(SCCD_MAX_ITER * 3*3*3, tol, sv,
                 // s1, s2, s3, ev, e1, e2, e3, t, u, v))
                 {
                     toi[i] = t;
@@ -82,86 +85,79 @@ namespace sccd {
         return min_t;
     }
 
+    template <typename T>
+    T narrow_phase_ee(const size_t noverlaps,
+                      const idx_t* const SFEM_RESTRICT e0overalp,
+                      const idx_t* const SFEM_RESTRICT e1overalp,
+                      // Geometric data
+                      T** const SFEM_RESTRICT v0,
+                      T** const SFEM_RESTRICT v1,
+                      const size_t edge_stride,
+                      idx_t** const SFEM_RESTRICT edges,
+                      // Output
+                      T* const SFEM_RESTRICT toi) {
+        const T infty = 100000;
+        T min_t = infty;
 
-        template <typename T>
-        T narrow_phase_ee(const size_t noverlaps,
-                          const idx_t* const SFEM_RESTRICT e0overalp,
-                          const idx_t* const SFEM_RESTRICT e1overalp,
-                          // Geometric data
-                          T** const SFEM_RESTRICT v0,
-                          T** const SFEM_RESTRICT v1,
-                          const size_t edge_stride,
-                          idx_t** const SFEM_RESTRICT edges,
-                          // Output
-                          T* const SFEM_RESTRICT toi) {
-            const T infty = 100000;
-            T min_t = infty;
+        int USE_TI = 0;
+        SFEM_READ_ENV(USE_TI, atoi);
 
-            int USE_TI = 0;
-            SFEM_READ_ENV(USE_TI, atoi);
+        int SCCD_MAX_ITER = 15;
+        SFEM_READ_ENV(SCCD_MAX_ITER, atoi);
 
-            tbb::parallel_for(tbb::blocked_range<size_t>(0, noverlaps), [&](const tbb::blocked_range<size_t>& r) {
-                std::vector<Box<double>> stack;
-                for (size_t i = r.begin(); i < r.end(); i++) {
-                    // for (size_t i = 0; i < noverlaps; i++) {
-                    const idx_t i0 = e0overalp[i];
-                    const idx_t i1 = e1overalp[i];
+        double tol = 1e-12;
 
-                    idx_t nodes0[2] = {edges[0][i0 * edge_stride], edges[1][i0 * edge_stride]};
-                    idx_t nodes1[2] = {edges[0][i1 * edge_stride], edges[1][i1 * edge_stride]};
+        tbb::parallel_for(tbb::blocked_range<size_t>(0, noverlaps), [&](const tbb::blocked_range<size_t>& r) {
+            std::vector<Box<double>> stack;
+            for (size_t i = r.begin(); i < r.end(); i++) {
+                // for (size_t i = 0; i < noverlaps; i++) {
+                const idx_t i0 = e0overalp[i];
+                const idx_t i1 = e1overalp[i];
 
-                    const double s1[3] = {v0[0][nodes0[0]], v0[1][nodes0[0]], v0[2][nodes0[0]]};
-                    const double s2[3] = {v0[0][nodes0[1]], v0[1][nodes0[1]], v0[2][nodes0[1]]};
+                idx_t nodes0[2] = {edges[0][i0 * edge_stride], edges[1][i0 * edge_stride]};
+                idx_t nodes1[2] = {edges[0][i1 * edge_stride], edges[1][i1 * edge_stride]};
 
-                    const double s3[3] = {v0[0][nodes1[2]], v0[1][nodes1[2]], v0[2][nodes1[2]]};
-                    const double s4[3] = {v0[0][nodes1[2]], v0[1][nodes1[2]], v0[2][nodes1[2]]};
+                const double s1[3] = {v0[0][nodes0[0]], v0[1][nodes0[0]], v0[2][nodes0[0]]};
+                const double s2[3] = {v0[0][nodes0[1]], v0[1][nodes0[1]], v0[2][nodes0[1]]};
 
-                    const double e1[3] = {v1[0][nodes0[0]], v1[1][nodes0[0]], v1[2][nodes0[0]]};
-                    const double e2[3] = {v1[0][nodes0[1]], v1[1][nodes0[1]], v1[2][nodes0[1]]};
+                const double s3[3] = {v0[0][nodes1[2]], v0[1][nodes1[2]], v0[2][nodes1[2]]};
+                const double s4[3] = {v0[0][nodes1[2]], v0[1][nodes1[2]], v0[2][nodes1[2]]};
 
-                    const double e3[3] = {v1[0][nodes1[2]], v1[1][nodes1[2]], v1[2][nodes1[2]]};
-                    const double e4[3] = {v1[0][nodes1[2]], v1[1][nodes1[2]], v1[2][nodes1[2]]};
+                const double e1[3] = {v1[0][nodes0[0]], v1[1][nodes0[0]], v1[2][nodes0[0]]};
+                const double e2[3] = {v1[0][nodes0[1]], v1[1][nodes0[1]], v1[2][nodes0[1]]};
 
+                const double e3[3] = {v1[0][nodes1[2]], v1[1][nodes1[2]], v1[2][nodes1[2]]};
+                const double e4[3] = {v1[0][nodes1[2]], v1[1][nodes1[2]], v1[2][nodes1[2]]};
 
-                    // Iteration variables
-                    double t = 0;
-                    double u = 0;
-                    double v = 0;
-                    double tol = 1e-12;
-                    int max_iter = 15;
+                // Iteration variables
+                double t = 0;
+                double u = 0;
+                double v = 0;
 
-    #ifdef SCCD_ENABLE_TIGHT_INCLUSION
-    #warning "SCCD_ENABLE_TIGHT_INCLUSION"
-                    if (USE_TI) {
-                        if (find_root_tight_inclusion_ee<double>(
-                                max_iter * 1000, tol, 
-                                s1, s2, s3, s4, 
-                                e1, e2, e3, e4, 
-                                t, u, v)) 
-                        {
-                            toi[i] = t;
-                            min_t = sccd::min<T>(t, min_t);
-                        } else {
-                            toi[i] = infty;
-                        }
-                        continue;
-                    }
-    #endif
-                    if (find_root_grid_ee<double>(max_iter, tol, 
-                        s1, s2, s3, s4, 
-                        e1, e2, e3, e4, 
-                        t, u, v, stack))
-                    {
+#ifdef SCCD_ENABLE_TIGHT_INCLUSION
+#warning "SCCD_ENABLE_TIGHT_INCLUSION"
+                if (USE_TI) {
+                    if (find_root_tight_inclusion_ee<double>(
+                            SCCD_MAX_ITER * 1000, tol, s1, s2, s3, s4, e1, e2, e3, e4, t, u, v)) {
                         toi[i] = t;
                         min_t = sccd::min<T>(t, min_t);
                     } else {
                         toi[i] = infty;
                     }
+                    continue;
                 }
-            });
+#endif
+                if (find_root_grid_ee<double>(SCCD_MAX_ITER, tol, s1, s2, s3, s4, e1, e2, e3, e4, t, u, v, stack)) {
+                    toi[i] = t;
+                    min_t = sccd::min<T>(t, min_t);
+                } else {
+                    toi[i] = infty;
+                }
+            }
+        });
 
-            return min_t;
-        }
+        return min_t;
+    }
 
 }  // namespace sccd
 
