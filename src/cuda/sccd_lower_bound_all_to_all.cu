1488e3607ca8a0e9958437e5687ffdb5b79b4c48
Patrick Zulian <patrick.zulian@gmail.com>
2026-05-06 11:32:12 +0200

    ...

--- src/cuda/sccd_lower_bound_all_to_all.cu
+++ src/cuda/sccd_lower_bound_all_to_all.cu
@@ -7,28 +7,76 @@
 #include <cstddef>
 
 namespace sccd {
+
     namespace host {
+        template <typename T, typename I>
+        void lower_bound_progressive(const ptrdiff_t count_search_keys,
+                                     const T* const SCCD_RESTRICT sorted_search_keys,
+                                     const ptrdiff_t count_sorted_keys,
+                                     const T* const SCCD_RESTRICT sorted_keys,
+                                     I* const SCCD_RESTRICT indices) {
+            auto first = std::lower_bound(sorted_keys, sorted_keys + count_sorted_keys, sorted_search_keys[0]);
+            if (first == sorted_keys + count_sorted_keys) {
+                for (ptrdiff_t i = 0; i < count_search_keys; i++) {
+                    indices[i] = count_sorted_keys;
+                }
+                return;
+            }
+
+            for (ptrdiff_t i = 1; i < count_search_keys; i++) {
+                for (ptrdiff_t j = indices[i - 1]; j < count_sorted_keys; j++) {
+                    if (sorted_keys[j] >= sorted_search_keys[i]) {
+                        indices[i] = j;
+                        break;
+                    }
+                }
+            }
+        }
+
         template <typename T, typename I>
         void lower_bound_all_to_all(const ptrdiff_t count_search_keys,
                                     const T* const SCCD_RESTRICT sorted_search_keys,
                                     const ptrdiff_t count_sorted_keys,
                                     const T* const SCCD_RESTRICT sorted_keys,
                                     I* const SCCD_RESTRICT indices) {
+#if 1
+            //             static const ptrdiff_t TILE_SIZE = 128;
+            // #pragma omp parallel for
+            //             for (ptrdiff_t i = 0; i < count_search_keys; i += TILE_SIZE) {
+            //                 ptrdiff_t iend = min(i + TILE_SIZE, count_search_keys);
+
+            //                 ptrdiff_t start = 0;
+            //                 for (ptrdiff_t j = i; j < iend; j++) {
+            //                     start =
+            //                         std::lower_bound(sorted_keys + start, sorted_keys + count_sorted_keys,
+            //                         sorted_search_keys[j]) - sorted_keys;
+            //                     indices[j] = start;
+            //                 }
+            //             }
+
+            static const ptrdiff_t TILE_SIZE = 128;
+#pragma omp parallel for
+            for (ptrdiff_t tile = 0; tile < count_search_keys; tile += TILE_SIZE) {
+                ptrdiff_t iend = std::min(tile + TILE_SIZE, count_search_keys);
+                lower_bound_progressive(
+                    iend - tile, sorted_search_keys + tile, count_sorted_keys, sorted_keys, indices + tile);
+            }
+#else
+
 #pragma omp parallel for
             for (ptrdiff_t i = 0; i < count_search_keys; i++) {
-                ptrdiff_t start = (i == 0) ? 0 : indices[i - 1];
                 indices[i] =
                     std::lower_bound(sorted_keys, sorted_keys + count_sorted_keys, sorted_search_keys[i]) - sorted_keys;
             }
+
+#endif
         }
     }  // namespace host
 
     namespace device {
 
         template <typename T>
-        __device__ __forceinline__ const T* lower_bound(const T* const SCCD_RESTRICT begin,
-                                                        const T* const SCCD_RESTRICT end,
-                                                        const T val) {
+        __device__ __forceinline__ const T* lower_bound(const T* const begin, const T* const end, const T val) {
             ptrdiff_t count = end - begin;
             const T* it = begin;
 
