deea0e4be4d3f6fa517ed2b4b5f32a6ec7dba5e9
Patrick Zulian <patrick.zulian@gmail.com>
2026-05-21 21:13:47 +0200

    added legacy API

--- src/narrowphase.hpp
+++ src/narrowphase.hpp
@@ -196,6 +196,38 @@ namespace sccd {
         return 0;
     }
 
+    // Legacy direct-call overload: the old API only exposed toi_stride here.
+    template <int nxe, typename T, typename I>
+    int narrow_phase_vf(const size_t noverlaps,
+                        const I* const SCCD_RESTRICT voveralp,
+                        const I* const SCCD_RESTRICT foveralp,
+                        T** const SCCD_RESTRICT v0,
+                        T** const SCCD_RESTRICT v1,
+                        const size_t face_stride,
+                        I** const SCCD_RESTRICT faces,
+                        const T max_toi,
+                        T* const SCCD_RESTRICT toi,
+                        const int toi_stride = 0) {
+        int SCCD_MAX_DEPTH = 69;
+        SCCD_READ_ENV(SCCD_MAX_DEPTH, atoi);
+
+        T SCCD_TOL = T(3e-8);
+        SCCD_READ_ENV(SCCD_TOL, atof);
+
+        return narrow_phase_vf<nxe, T, I>(noverlaps,
+                                          voveralp,
+                                          foveralp,
+                                          v0,
+                                          v1,
+                                          face_stride,
+                                          faces,
+                                          max_toi,
+                                          toi,
+                                          SCCD_MAX_DEPTH,
+                                          SCCD_TOL,
+                                          toi_stride);
+    }
+
     template <typename T, typename I>
     int narrow_phase_ee(const size_t noverlaps,
                         const I* const SCCD_RESTRICT e0overalp,
@@ -339,6 +371,38 @@ namespace sccd {
 
         if (toi_stride == 0) toi[0] = min_t;
         return 0;
+    }
+
+    // Legacy direct-call overload: the old API only exposed toi_stride here.
+    template <typename T, typename I>
+    int narrow_phase_ee(const size_t noverlaps,
+                        const I* const SCCD_RESTRICT e0overalp,
+                        const I* const SCCD_RESTRICT e1overalp,
+                        T** const SCCD_RESTRICT v0,
+                        T** const SCCD_RESTRICT v1,
+                        const size_t edge_stride,
+                        I** const SCCD_RESTRICT edges,
+                        const T max_toi,
+                        T* const SCCD_RESTRICT toi,
+                        const int toi_stride = 0) {
+        int SCCD_MAX_DEPTH = 69;
+        SCCD_READ_ENV(SCCD_MAX_DEPTH, atoi);
+
+        T SCCD_TOL = T(3e-8);
+        SCCD_READ_ENV(SCCD_TOL, atof);
+
+        return narrow_phase_ee<T, I>(noverlaps,
+                                     e0overalp,
+                                     e1overalp,
+                                     v0,
+                                     v1,
+                                     edge_stride,
+                                     edges,
+                                     max_toi,
+                                     toi,
+                                     SCCD_MAX_DEPTH,
+                                     SCCD_TOL,
+                                     toi_stride);
     }
 
 }  // namespace sccd
